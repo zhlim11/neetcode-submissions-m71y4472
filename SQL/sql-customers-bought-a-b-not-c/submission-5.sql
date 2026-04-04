a7a68dd027c0f9769d96c3bdffe6a0910598717a
zhlim11 <66876989+zhlim11@users.noreply.github.com>
2026-04-04 09:09:28 +0800

    Add: sql-customers-bought-a-b-not-c - submission-5

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-5.sql
@@ -0,0 +1,16 @@
+-- Write your query below
+with joined AS (
+SELECT c.customer_id, c.customer_name, o.product_name
+from customers c
+left join orders o
+on c.customer_id = o.customer_id),
+
+agg AS (
+SELECT customer_id, customer_name, string_agg(product_name, ', ' ORDER BY product_name) AS product_list
+FROM joined
+GROUP BY customer_id, customer_name)
+
+SELECT customer_id, customer_name
+FROM agg
+WHERE product_list LIKE '%A%B%' AND product_list NOT LIKE '%C%'
+ORDER BY customer_name;
